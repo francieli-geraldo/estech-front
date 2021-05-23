f43e30ed57e1b937da1e057ef7a7a203f7d9a7b8
Francieli Ferreira Geraldo <francieligeraldo@ENGEFT01.LOCAL>
2021-05-23 16:18:27 -0300

    Relatórios - formulário inicial

--- src/app/modules/relatorios/relatorios.component.ts
+++ src/app/modules/relatorios/relatorios.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import KTWizard from '../../../assets/js/components/wizard';
+import { KTUtil } from '../../../assets/js/components/util';
 
 @Component({
   selector: 'app-relatorios',
   templateUrl: './relatorios.component.html',
   styleUrls: ['./relatorios.component.scss']
 })
-export class RelatoriosComponent implements OnInit {
+export class RelatoriosComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('wizard', { static: true }) el: ElementRef;
 
-  ngOnInit(): void {
+  model: any = {
+    relatorio: "",
+    status:"",
+    grupo: "",
+    semana: ""
+  };
+
+  isDisabled: boolean = false;
+  submitted = false;
+  wizard: any;
+
+  constructor() {
   }
 
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    // Initialize form wizard
+    this.wizard = new KTWizard(this.el.nativeElement, {
+      startStep: 1
+    });
+
+    // Validation before going to next page
+    this.wizard.on('beforeNext', (wizardObj) => {
+      // https://angular.io/guide/forms
+      // https://angular.io/guide/form-validation
+
+      // validate the form and use below function to stop the wizard's step
+      // wizardObj.stop();
+    });
+   
+  }
+
+  isCheck() {
+    this.model.relatorio == '1'
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  ngOnDestroy() {
+    this.wizard = undefined;
+  }
 }
