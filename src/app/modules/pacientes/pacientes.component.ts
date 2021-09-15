9a9bd2ea5e39fc15327cb25af87bb78193c9a940
Francieli Ferreira Geraldo <francieligeraldo@engefoto.com.br>
2021-09-14 21:02:12 -0300

    Filtrar paciente

--- src/app/modules/pacientes/pacientes.component.ts
+++ src/app/modules/pacientes/pacientes.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {
   GroupingState,
@@ -37,10 +37,7 @@ export class PacientesComponent
   ICreateAction,
   IEditAction,
   ISortView,
-  IFilterView,
-  IGroupingView,
-  ISearchView,
-  IFilterView {
+  IGroupingView {
   paginator: PaginatorState;
   sorting: SortState;
   grouping: GroupingState;
@@ -57,7 +54,6 @@ export class PacientesComponent
 
   // angular lifecircle hooks
   ngOnInit(): void {
-    this.filterForm();
     this.searchForm();
     this.service.fetch();
     this.grouping = this.service.grouping;
@@ -73,53 +69,20 @@ export class PacientesComponent
     this.subscriptions.forEach((sb) => sb.unsubscribe());
   }
 
-  // filtration
-  filterForm() {
-    this.filterGroup = this.fb.group({
-      status: [''],
-      type: [''],
-      searchTerm: [''],
-    });
-    this.subscriptions.push(
-      this.filterGroup.controls.status.valueChanges.subscribe(() =>
-        this.filter()
-      )
-    );
-    this.subscriptions.push(
-      this.filterGroup.controls.type.valueChanges.subscribe(() => this.filter())
-    );
-  }
-
-  filter() {
-    const filter = {};
-    const status = this.filterGroup.get('status').value;
-    if (status) {
-      filter['status'] = status;
-    }
-
-    const type = this.filterGroup.get('type').value;
-    if (type) {
-      filter['type'] = type;
-    }
-    this.service.patchState({ filter });
-  }
 
   // search
   searchForm() {
     this.searchGroup = this.fb.group({
-      searchTerm: [''],
+      searchTerm: [""],
     });
     const searchEvent = this.searchGroup.controls.searchTerm.valueChanges
       .pipe(
-        debounceTime(150),
-        distinctUntilChanged()
+        map(value => value.trim()),
+        debounceTime(400),
+        distinctUntilChanged(),
       )
-      .subscribe((val) => this.search(val));
+      .subscribe((val) => this.service.patchState({ filter: { search: val } })); 
     this.subscriptions.push(searchEvent);
-  }
-
-  search(searchTerm: string) {
-    this.service.patchState({ searchTerm });
   }
 
   // sorting
