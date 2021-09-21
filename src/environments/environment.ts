3ab00dc39a27318e314101fa2e582237d99ed3d5
Samuel Silva da Cruz <samuel.cruz@meta.com.br>
2021-09-20 22:33:22 -0300

    removido imagens não utilizadas e atualizado a url do backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v726demo2',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'https://696f-190-89-107-7.ngrok.io/v1'
+  apiUrl: 'https://5c5c-190-89-107-7.ngrok.io/v1'
 };
 
 /*
