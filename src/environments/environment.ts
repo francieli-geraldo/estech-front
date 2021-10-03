32beaf32e766aec9ab99eacd1ee9d08e8ac2cb7e
Francieli Ferreira Geraldo <francieligeraldo@engefoto.com.br>
2021-10-02 22:24:18 -0300

    imagens não utilizadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appVersion: 'v726demo2',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: false,
-  apiUrl: 'https://173d-190-89-107-7.ngrok.io/v1'
+  apiUrl: 'https://ef25-190-89-107-7.ngrok.io/v1'
 };
 
 /*
